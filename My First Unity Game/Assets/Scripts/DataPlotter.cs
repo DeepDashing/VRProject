bd9a324f6ee0f929cbd384c0db636dd0ad27a543
Flashfire23 <Vanisse23@gmail.com>
2019-11-05 14:33:22 -0700

    Hockey

---
+++ My First Unity Game/Assets/Scripts/DataPlotter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlotter : MonoBehaviour
+{
+    public GameObject PointPrefab;
+    public Material red;
+    public Material blue;
+    public int maxPoints;
+    
+
+    private List<int> pointList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instantiate prefab
+        //(PointPrefab, new Vector3(0,0,0), Quaternion.identity);
+
+        pointList = GenerateData();
+
+        //Log to console
+        foreach (int val in pointList)
+        {
+            Debug.Log(val);
+        }
+        
+        //loop through Pointlist
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            //Get value in pointList
+            float x = System.Convert.ToSingle(i);
+            float y = System.Convert.ToSingle(pointList[i]);
+            float z = System.Convert.ToSingle(0);
+           
+
+
+            if (y > 1)
+            {
+                PointPrefab.GetComponent<Renderer>().material = red;
+            }
+
+            else
+            {
+                PointPrefab.GetComponent<Renderer>().material = blue;
+            }
+            
+
+            
+
+            //instantiate the prefab with coordinates defined above
+            Instantiate(PointPrefab, new Vector3(x, y, z), Quaternion.identity);
+
+            
+        }
+    }
+
+    public List<int> GenerateData()
+    {
+        List<int> myData = new List<int>();
+
+        int fakeNumber;
+        for (int i = 0; i < maxPoints; i++)
+        {
+            fakeNumber = System.Convert.ToInt32(Random.Range(0, 10));
+            myData.Add(fakeNumber);
+        }
+
+        return myData;
+    }
+}
---
+++ My First Unity Game/Assets/Scripts/KeyboardControll.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControll : MonoBehaviour
+{
+    Vector3 originalPos;
+    public float speed;
+    private Rigidbody rb;
+    public GameObject PreFabe;
+
+    void Start()
+    {
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        //alternatively, just: originalPos = gameObject.transform.position
+        originalPos = new Vector3(PreFabe.transform.position.x, PreFabe.transform.position.y, PreFabe.transform.position.z);
+        rb = GetComponent<Rigidbody>();
+    }
+    void Awake()
+    {
+        PreFabe.GetComponent<KeyboardControll>().enabled = true;
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        //alternatively, just: originalPos = gameObject.transform.position
+        originalPos = new Vector3(PreFabe.transform.position.x, PreFabe.transform.position.y, PreFabe.transform.position.z);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    
+
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            Destroy(gameObject);
+            gameObject.transform.position = originalPos;
+            Instantiate(PreFabe, originalPos, Quaternion.Euler(0, 0, 0));
+            
+        }
+
+    }
+}
+
